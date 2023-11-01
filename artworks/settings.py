8378726c3a9194ea090c117af2094caef6a6355d
Ubuntu <ubuntu@ip-172-31-57-131.ec2.internal>
2023-11-01 10:44:04 +0000

    alejandro

--- artworks/settings.py
+++ artworks/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
     'collection.apps.CollectionConfig',
+    'artworks',
     'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
