5fd7439bc979bd7f52cd32a5bbc262279bb248d5
Ubuntu <ubuntu@ip-172-31-57-131.ec2.internal>
2023-11-07 13:16:32 +0000

    nuevo avance

--- collection/urls.py
+++ collection/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("artwork/<int:artwork_id>", views.artwork, name="artwork"),
+    path("search/artworks/", views.search_artworks, name="search_artworks"),
     path("accounts/profile/", views.index, name="index"),
     path("accounts/register/", views.register, name="register")
 ]
--- collection/views.py
+++ collection/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 import random
 from collection.models import Artwork
+from django.contrib.postgres import search
+from django.core.paginator import Paginator
+
 
 def register(request):
     if request.method == 'POST':
@@ -37,3 +40,39 @@ def index(request):
         random_works = random.sample(artworks, 12)
 
     return render(request, 'collection/index.html', {'artworks': random_works})
+
+
+def search_artworks(request):
+    if request.method == 'GET':
+        value = request.GET['search']
+        artworks = ft_artworks(value)
+
+        paginator = Paginator(artworks, 8) 
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'collection/index.html',
+                      {'artworks': artworks, 'search_value': value, 
+                       "page_obj": page_obj})
+    else:
+        return render(request, 'collection/index.html',
+                      {'artworks': [], 'search_value': None})
+
+
+def ft_artworks(value):
+    vector = (
+        search.SearchVector("title", weight="A")
+        + search.SearchVector("author__name", weight="B")
+        + search.SearchVector("style__name", weight="C")
+        + search.SearchVector("genre__name", weight="C")
+    )
+    query = search.SearchQuery(value, search_type="websearch")
+    return (
+        Artwork.objects.annotate(
+            search=vector,
+            rank=search.SearchRank(vector, query),
+        )
+        .filter(search=query)
+        .order_by("-rank")
+    
+    )
